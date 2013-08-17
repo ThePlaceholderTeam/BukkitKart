f14a6f337f28801f5d526e3655d7ad82ad677815
Kuuichi <kuuichin@gmail.com>
2013-08-16 22:22:43 -0700

    basic commit

---
+++ BukkitKart/src/net/theplaceholderteam/bukkitkart/BukkitKart.java
@@ -0,0 +1,15 @@
+package net.theplaceholderteam.bukkitkart;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BukkitKart extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+
+}
